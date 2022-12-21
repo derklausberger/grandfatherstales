4e83efe67df3ffd108120a178f71c451d056acdb
KilianHnidek <kilian.hnidek@stud.fh-campuswien.ac.at>
2022-12-21 22:23:56 +0100

    Modifications in StartMenu: Background, Added Resource Files

--- src/main/java/StartMenu.java
+++ src/main/java/StartMenu.java
@@ -1,7 +1,14 @@
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 
 public class StartMenu extends JFrame {
+    public static final int WINDOW_WIDTH = 1280;
+    public static final int WINDOW_HEIGHT = 920;
+
     private JPanel mainPanel;
+    public JLabel background;
 
     public StartMenu(String title) {
         super(title);
@@ -9,11 +16,19 @@ public class StartMenu extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+
+        background.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                background.setIcon(new ImageIcon("src/main/resources/MainMenu2.png"));
+            }
+        });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new StartMenu("Titel");
+        JFrame frame = new StartMenu("game");
         frame.setVisible(true);
-        frame.setSize(200, 200);
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.pack();
     }
 }
---
+++ src/main/resources/tiled/tiles_dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tiles_dungeon" tilewidth="16" tileheight="16" tilecount="299" columns="23">
+ <image source="../map16x16/.png/tiles_dungeon.png" width="368" height="208"/>
+</tileset>
