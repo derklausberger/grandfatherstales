f0acd162ebdf18ee59a4a645f930743571063efd
derklausberger <niklasklausberger@gmail.com>
2023-01-22 17:34:40 +0100

    fixed exit door

--- src/main/java/objectClasses/Level.java
+++ src/main/java/objectClasses/Level.java
@@ -193,7 +193,7 @@ public class Level {
         enemies = new ArrayList<>();
         Random random = new Random();
         // Biome, Level, Number of enemies should be contained in the xml file
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < id * 5; i++) {
             int x, y;
             do {
                 x = random.nextInt(32) * GamePanel.NEW_TILE_SIZE;
